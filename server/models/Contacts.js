ed684e8a4114562661adf958699083fbacb94164
avezqureshi14 <aveznqureshi14@gmail.com>
2024-02-04 18:10:37 +0530

    fix: contact controller

--- server/models/Contacts.js
+++ server/models/Contacts.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 const contactSchema = new mongoose.Schema({
   firstName:{
     type:String,
+    required:true,
   },
   lastName:{
     type:String,
+    required:true,
   },
   phoneNumber:{
     type:String,
+    required:true
   }
 });
 
